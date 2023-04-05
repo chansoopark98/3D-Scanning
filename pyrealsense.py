50bc34163da2c99043c72adcb371aae6a834aab2
park <chansoo0710@gmail.com>
2023-04-06 00:59:31 +0900

    Update: RealSense D435 based RGB-D SLAM

--- pyrealsense.py
+++ pyrealsense.py
@@ -1,6 +1,46 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+from enum import IntEnum
+
+class Preset(IntEnum):
+    Custom = 0
+    Default = 1
+    Hand = 2
+    HighAccuracy = 3
+    HighDensity = 4
+    MediumDensity = 5
+
+def get_profiles():
+    ctx = rs.context()
+    devices = ctx.query_devices()
+
+    color_profiles = []
+    depth_profiles = []
+    for device in devices:
+        name = device.get_info(rs.camera_info.name)
+        serial = device.get_info(rs.camera_info.serial_number)
+        print('Sensor: {}, {}'.format(name, serial))
+        print('Supported video formats:')
+        for sensor in device.query_sensors():
+            for stream_profile in sensor.get_stream_profiles():
+                stream_type = str(stream_profile.stream_type())
+
+                if stream_type in ['stream.color', 'stream.depth']:
+                    v_profile = stream_profile.as_video_stream_profile()
+                    fmt = stream_profile.format()
+                    w, h = v_profile.width(), v_profile.height()
+                    fps = v_profile.fps()
+
+                    video_type = stream_type.split('.')[-1]
+                    print('  {}: width={}, height={}, fps={}, fmt={}'.format(
+                        video_type, w, h, fps, fmt))
+                    if video_type == 'color':
+                        color_profiles.append((w, h, fps, fmt))
+                    else:
+                        depth_profiles.append((w, h, fps, fmt))
+
+    return color_profiles, depth_profiles
 
 class PyRealSenseCamera(object):
     def __init__(self):
@@ -18,8 +58,8 @@ class PyRealSenseCamera(object):
         [Open3D INFO]   color_resolution: [1280,720 | 1920,1080 | 320,180 | 320,240 | 424,240 | 640,360 | 640,480 | 848,480 | 960,540]
         [Open3D INFO]   color_format: [RS2_FORMAT_BGR8 | RS2_FORMAT_BGRA8 | RS2_FORMAT_RGB8 | RS2_FORMAT_RGBA8 | RS2_FORMAT_Y16 | RS2_FORMAT_YUYV]
         """
-        self.config.enable_stream(rs.stream.depth, 1280,720, rs.format.z16, 100)
-        self.config.enable_stream(rs.stream.color, 1920,1080, rs.format.bgr8, 60)
+        self.config.enable_stream(rs.stream.depth, 640,480, rs.format.z16, 30)
+        self.config.enable_stream(rs.stream.color, 1920, 1080, rs.format.rgb8, 30)
 
         # Start the stream
         self.profile = self.pipeline.start(self.config)
@@ -40,6 +80,13 @@ class PyRealSenseCamera(object):
 
         self.capture()
 
+    def get_profile(self):
+        color_profiles, depth_profiles = get_profiles()
+        print('Using the default profiles: \n  color:{}, depth:{}'.format(
+            color_profiles[0], depth_profiles[0]))
+        print('depth_profiles', depth_profiles[0])
+        print('color_profiles', color_profiles[0])
+
     def capture(self):
         self.frames = self.pipeline.wait_for_frames()
         self.aligned_frames = self.align.process(self.frames)
@@ -48,7 +95,6 @@ class PyRealSenseCamera(object):
 
     def get_color(self) -> np.ndarray:
         self.color_image = np.asanyarray(self.color_frame.get_data()).astype('uint8')
-        self.color_image = cv2.cvtColor(self.color_image, cv2.COLOR_BGR2RGB)
         return self.color_image
 
     def get_depth(self) -> np.ndarray:
@@ -61,10 +107,23 @@ class PyRealSenseCamera(object):
             _ = self.get_color()
 
         color_intrinsics = self.color_frame.profile.as_video_stream_profile().intrinsics
-        
+
         K = np.array([
             [color_intrinsics.fx, 0, color_intrinsics.ppx],
             [0, color_intrinsics.fy, color_intrinsics.ppy],
             [0, 0, 1]
         ])
         return K
+
+if __name__ == '__main__':
+    camera = PyRealSenseCamera()
+    camera.get_profile()
+    while True:
+        camera.capture()
+        rgb = camera.get_color()
+        depth = camera.get_depth()
+        expand_depth = np.expand_dims(depth, axis=-1)
+        rgb = np.where(expand_depth==0, 127, rgb) 
+        
+        cv2.imshow('test', rgb)
+        cv2.waitKey(1)
--- realtime_slam_realsense.py
+++ realtime_slam_realsense.py
@@ -8,11 +8,12 @@ from slam_common import save_poses, extract_trianglemesh
 
 depth_scale = 1000.0 # 1000.0
 depth_min = 0.1 # 0.1
-depth_max = 10.0 # 3.0
+depth_max = 3.0 # 3.0
 odometry_distance_thr = 0.07 # 0.07
 trunc_voxel_multiplier = 8.0 # 8.0
-voxel_size = 0.001 # 0.001
-block_count = 10000 # 40000
+voxel_size = 0.002 # 0.001
+block_count = 40000 # 40000
+ray_cast = True # False
 
 def slam(depth_file_names, color_file_names, intrinsic):
     n_files = len(color_file_names)
@@ -23,6 +24,7 @@ def slam(depth_file_names, color_file_names, intrinsic):
                                        block_count, T_frame_to_model,
                                        device)
     depth_ref = o3d.t.geometry.Image((depth_file_names[0]))
+    color_ref = o3d.t.geometry.Image((color_file_names[0]))
 
     print(depth_ref.rows, 'rows')
     print(depth_ref.columns, 'columns')
@@ -30,6 +32,12 @@ def slam(depth_file_names, color_file_names, intrinsic):
     input_frame = o3d.t.pipelines.slam.Frame(depth_ref.columns, depth_ref.rows, 
                                              intrinsic, device)
     raycast_frame = o3d.t.pipelines.slam.Frame(depth_ref.columns, depth_ref.rows, intrinsic,device)
+
+    input_frame.set_data_from_image('depth', depth_ref)
+    input_frame.set_data_from_image('color', color_ref)
+
+    raycast_frame.set_data_from_image('depth', depth_ref)
+    raycast_frame.set_data_from_image('color', color_ref)
 
     poses = []
 
@@ -41,10 +49,6 @@ def slam(depth_file_names, color_file_names, intrinsic):
 
         input_frame.set_data_from_image('depth', depth)
         input_frame.set_data_from_image('color', color)
-
-        raycast_frame.set_data_from_image('depth', depth)
-        raycast_frame.set_data_from_image('color', color)
-
 
         if i > 0:
             result = model.track_frame_to_model(input_frame, raycast_frame,
@@ -60,7 +64,7 @@ def slam(depth_file_names, color_file_names, intrinsic):
                         trunc_voxel_multiplier)
         model.synthesize_model_frame(raycast_frame, depth_scale,
                                      depth_min, depth_max,
-                                     trunc_voxel_multiplier, False)
+                                     trunc_voxel_multiplier, ray_cast)
         stop = time.time()
         print('{:04d}/{:04d} slam takes {:.4}s'.format(i, n_files,
                                                        stop - start))
@@ -70,7 +74,8 @@ def slam(depth_file_names, color_file_names, intrinsic):
 if __name__ == '__main__':
     print(o3d.t.io.RealSenseSensor.list_devices())
     camera = PyRealSenseCamera()
-    # camera.capture()
+    camera.capture()
+    time.sleep(2)
     rgb = camera.get_color()[:, :, :3].astype(np.uint8)
     intrinsic_matrix = camera.get_camera_intrinsic()
     
@@ -88,38 +93,36 @@ if __name__ == '__main__':
                                                           fy=fy,
                                                           cx=cx,
                                                           cy=cy)
-
     camera_intrinsics = o3d.core.Tensor(camera_intrinsics.intrinsic_matrix,
                                o3d.core.Dtype.Float64)
+
+    print(camera_intrinsics)
+
     
     rgb_list = []
     depth_list = []
 
-    camera.capture()
-
-    for i in range(30):
+    for i in range(300):
         print('capture')
         camera.capture()
+        # time.sleep(0.5)
         
-        rgb_image = camera.get_color()[:, :, :3].astype(np.uint8) / 255
-        depth_image = camera.get_depth() #/ 1000.
+        rgb_image = camera.get_color()
+        depth_image = camera.get_depth()
 
-        if i !=0:
-            #depth_image = np.where(depth_image>=3.0, 0, depth_image)
-            plt.imshow(rgb_image)
-            plt.show()
-            plt.imshow(depth_image)
-            plt.show()
+        rgb_image = rgb_image.astype('float32') / 255.
+        depth_image = depth_image.astype('float32')
+        
 
-            
-            rgb_image = rgb_image.astype('float32')
-            depth_image = depth_image.astype('float32')
 
-            rgb_list.append(rgb_image)
-            depth_list.append(depth_image)
-            # time.sleep(0.)
+        rgb_list.append(rgb_image)
+        depth_list.append(depth_image)
+        # time.sleep(0.5)
     
     model, volume, poses = slam(depth_file_names=depth_list, color_file_names=rgb_list, intrinsic=camera_intrinsics)
-    pcd = model.extract_pointcloud().to_legacy()
+    # pcd = model.extract_pointcloud(3.0).to_legacy()
+    pcd = model.voxel_grid.extract_point_cloud(
+                    3.0, 6000000).to_legacy()
     print(pcd)
+    print(poses)
     o3d.visualization.draw_geometries([pcd])
